f1065dadb3703556e8c7b02316635c6f8ef06f04
Jack Li <liujin834@gmail.com>
2023-02-16 15:47:58 -0500

    feat: new k8s configs

--- freeyeti/settings/base.py
+++ freeyeti/settings/base.py
@@ -90,9 +90,16 @@ WSGI_APPLICATION = "freeyeti.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PWD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'TEST': {
+            'NAME': os.environ.get('TEST_DB_NAME')
+        },
     }
 }
 
--- freeyeti/settings/production.py
+++ freeyeti/settings/production.py
@@ -2,6 +2,9 @@ from .base import *
 
 DEBUG = False
 
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 try:
     from .local import *
 except ImportError:
