7e1d4fd8ea491b4660b2bd321b16a98a8d13585f
Jack Li <liujin834@gmail.com>
2023-02-21 16:17:01 -0500

    feat: add logging

--- freeyeti/settings/production.py
+++ freeyeti/settings/production.py
@@ -2,8 +2,34 @@ from .base import *
 
 DEBUG = False
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'formatter': 'verbose',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.environ.get('DJANGO_LOG_FILE'),
+            'when': 'midnight',
+            'interval': 1
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
 
 try:
     from .local import *
