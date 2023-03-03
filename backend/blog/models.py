7c0445bd554bb3f4c57a67bdfbb816ba2b43c3c0
Jack Li <liujin834@gmail.com>
2023-03-03 11:43:29 -0500

    feat: serialize the blog body

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -4,16 +4,30 @@ from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.api import APIField
 
+from rest_framework import fields
+from wagtail.core.rich_text import expand_db_html
+
+
+class APIRichTextSerializer(fields.CharField):
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        return expand_db_html(representation)
+
+class APIRichTextField(APIField):
+    def __init__(self, name):
+        serializer = APIRichTextSerializer()
+        super().__init__(name=name, serializer=serializer)
 
 class BlogPage(Page):
     body = RichTextField(blank=True)
+    
     content_panels = Page.content_panels + [
         FieldPanel('body'),
     ]
 
     # Export fields over the API
     api_fields = [
-        APIField('body'),
+        APIRichTextField('body'),
     ]
 
 class BlogIndex(Page):
