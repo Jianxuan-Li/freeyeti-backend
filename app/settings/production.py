d5306a794eec4a77ae2219e429960cc7ebda25bf
Jack Li <liujin834@gmail.com>
2023-03-16 14:25:05 -0400

    feat: optimize the settings for production

--- app/settings/production.py
+++ app/settings/production.py
@@ -4,45 +4,59 @@ DEBUG = False
 
 # Logging
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': '{',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
         },
     },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'formatter': 'verbose',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.environ.get('DJANGO_LOG_FILE'),
-            'when': 'midnight',
-            'interval': 1
+    "handlers": {
+        "console": {
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "formatter": "verbose",
+            "class": "logging.StreamHandler",
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': True,
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": True,
         },
     },
 }
 
 # allowed hosts
-ALLOWED_HOSTS = ['freeyeti.net', 'www.freeyeti.net']
+ALLOWED_HOSTS = ["freeyeti.net", "www.freeyeti.net"]
 
 FORCE_SCRIPT_NAME = "/backend"
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://freeyeti.net',
-    'https://www.freeyeti.net',
-]
-
 STATIC_URL = "/backend/static/"
 MEDIA_URL = "/backend/media/"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://freeyeti.net",
+    "https://www.freeyeti.net",
+]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# database optimized for production
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PWD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "TEST": {"NAME": os.environ.get("TEST_DB_NAME")},
+        "CONN_MAX_AGE": 60,
+        "CONN_HEALTH_CHECKS": True,
+    }
+}
 
 try:
     from .local import *
